7ac5b71afcee1a637c108a0ab949f8293c522011
Dubkov <yevgeniydubkov@gmail.com>
2021-12-31 01:50:07 +0300

    done

---
+++ lesson6/js/ProductComponent.js
@@ -0,0 +1,20 @@
+Vue.component('products', {
+    props: ['products'],
+    template: `<div class="products">
+                <product v-for="item of products" 
+                :key="item.id_product" 
+                :product="item"></product>
+               </div>`
+});
+Vue.component('product', {
+    props: ['product', 'img'],
+    template: `
+            <div class="product-item">
+                <div class="desc">
+                    <h3>{{product.product_name}}</h3>
+                    <p>{{product.price}}</p>
+                    <button class="buy-btn" @click="$parent.$emit('add-product', product)">Купить</button>
+                </div>
+            </div>
+    `
+})
